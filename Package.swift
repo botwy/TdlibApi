c28d98aba4b19a8c0a400bc4760889af1cc8b159
botwy <botwy@mail.ru>
2021-05-23 19:59:44 +0300

    remove platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "TdlibAPI",
-    platforms: [.watchOS(.v6)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
